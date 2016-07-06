676268c7883bf5270fb19b0c5fc3326d3bde2aee
Julian Silva <JulianSilva@Julians-MacBook-Pro.local>
2016-07-06 14:25:06 -0400

    Loops File

---
+++ Loops.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+for i in 1...200 {
+    if i % 15 == 0 {
+        print("BooYa")
+    } else if i % 3 == 0 {
+        print("Boo")
+    } else if i % 5 == 0 {
+        print("Ya")
+    }
+}
+
+
